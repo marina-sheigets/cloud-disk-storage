1b1160c0b433158ab2ef5f7266bb09602f0b64e1
Marina Sheigets <marinasejgec34@gmail.com>
2023-11-06 13:54:33 +0200

    client: add animation while opening folder

--- client/src/components/disk/fileList/FileList.jsx
+++ client/src/components/disk/fileList/FileList.jsx
@@ -2,9 +2,10 @@ import { useSelector } from 'react-redux';
 import './filelist.less';
 import { getFiles } from '../../../selectors';
 import File from './file/File';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 function FileList() {
-	const files = useSelector(getFiles).map((file) => <File key={file._id} file={file} />);
+	const files = useSelector(getFiles);
 
 	return (
 		<div className='filelist'>
@@ -15,10 +16,16 @@ function FileList() {
 						<div className='filelist__date'>Date</div>
 						<div className='filelist__size'>Size</div>
 					</div>
-					{files}
+					<TransitionGroup>
+						{files.map((file) => (
+							<CSSTransition key={file._id} timeout={500} classNames={'file'}>
+								<File file={file} />
+							</CSSTransition>
+						))}
+					</TransitionGroup>
 				</>
 			) : (
-				<>There are no folders</>
+				<>There are no files</>
 			)}
 		</div>
 	);
