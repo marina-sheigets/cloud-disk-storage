10fb4f6a18d980dd61be472e51d7f63256e836e6
Marina Sheigets <marinasejgec34@gmail.com>
2023-11-06 17:35:28 +0200

    server: fix method

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -121,8 +121,7 @@ class FileController {
 		try {
 			const file = await File.findOne({ _id: req.query.id, user: req.user.id });
 
-			const path =
-				config.get('filePath') + '\\' + req.user.id + '\\' + file.path + '\\' + file.name;
+			const path = fileService.getPath(file);
 			if (fs.existsSync(path)) {
 				return res.download(path, file.name);
 			}
