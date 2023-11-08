051cf561e111d787a8315bf5a6f5d4f4c13de401
Marina Sheigets <marinasejgec34@gmail.com>
2023-11-08 17:14:55 +0200

    server:  fix __dirname

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ import fileUpload from 'express-fileupload';
 import 'dotenv/config';
 import filePathMiddleware from './middleware/filePath.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 app.use(fileUpload({}));
