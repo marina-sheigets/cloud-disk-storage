128136d7a83b96ebc75d979690423e55098f9dc3
Marina Sheigets <marinasejgec34@gmail.com>
2023-11-08 18:06:31 +0200

    server: cors

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,10 +1,8 @@
 function cors(req, res, next) {
-	res.setHeader(
-		'Access-Control-Allow-Origin',
-		'https://cloud-disk-storage-jh5eerlqz-marina-sheigets-projects.vercel.app'
-	);
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	next();
 }
 
